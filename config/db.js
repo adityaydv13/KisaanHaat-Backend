06e6abdb9b3129537d28502305aa9f90a7a6152a
Aditya Yadav <adityaydv6335@gmail.com>
2025-08-24 14:24:19 +0530

    added

--- config/db.js
+++ config/db.js
@@ -4,9 +4,9 @@ dotenv.config();
 const connectDB = async () => {
   try {
     // await mongoose.connect("mongodb+srv://hrao1428:rE7wbYMMYOZb3I5L@cluster0.foash.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-      await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      await mongoose.connect("mongodb://localhost:27017/kisaanHaa", {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     });
     console.log('MongoDB connected');
   } catch (error) {
